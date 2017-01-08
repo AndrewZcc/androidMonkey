918b610bf2f838d770c577e16b827c2b17464db5
zhchuch_m <zcckernal@gmail.com>
2017-01-08 11:43:46 +0800

    monkey-testing finish.

--- benchmark_autName.js
+++ benchmark_autName.js
@@ -17,27 +17,27 @@ weight-chart	✅
 
 5. tippy
 
-tippy_1.1.3
+tippy_1.1.3		✅
 
 -------------------------------------
 
 6. myexpense
 
-MyExpenses
+MyExpenses		✅
 
 7. mininote
 
-jp.gr.java_conf.hatalab.mnv_40_src
+jp.gr.java_conf.hatalab.mnv_40_src		✅
 
 8. mileage
 
-com.evancharlton.mileage_3110_src
+com.evancharlton.mileage_3110_src 		✅
 
 9. anymemo
 
-anymemo-stable-8.3
+anymemo-stable-8.3		✅
 
 10. sanity
 
-cri.sanity_21100_src
+cri.sanity_21100_src	✅
 
